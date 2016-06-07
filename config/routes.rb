83c32f796629c0b51f6f2c15ef1dfb22435ea8bf
saylerb <sayler.b@gmail.com>
2016-06-07 12:57:26 -0600

    add categories index

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
   resources :items, only: [:index]
 
+  resources :categories, only: [:index]
   get '/:category_title', to: 'categories#show'
+
+  root to: 'categories#index'
+
 end
