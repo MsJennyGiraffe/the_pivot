087e5e4c4d12593cf7643d70515a1ef63b0379ff
Colin Osborn <colin.osborn@gmail.com>
2016-06-06 19:36:31 -0600

    edit item model
    renamed name column to title
    add image_path column to items

--- db/schema.rb
+++ db/schema.rb
@@ -11,17 +11,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160607003301) do
+ActiveRecord::Schema.define(version: 20160607013427) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "items", force: :cascade do |t|
-    t.string   "name"
+    t.string   "title"
     t.integer  "price"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.string   "image_path"
   end
 
 end
