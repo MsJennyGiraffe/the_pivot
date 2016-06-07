d3c8258c96fbe020503b58c89c21e4aa2a60bc8a
Colin Osborn <colin.osborn@gmail.com>
2016-06-06 19:30:05 -0600

    seed file
    add seed file changes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+items = [ { name: "Pick Axe", price: 15, description: "Picks things",
+            name: "Wagon Wheel", price: 80, description: "Makes that wagon go",
+            name: "Canvas Cover", price: 10, description: "covers yer wagon",
+            name: "Ammunition", price: 100, description: "How else you gonna kill Buffalo?",
+            name: "Britches", price: 5, description: "Covers yer rear end"
+  } ]
+
+  items.each do |data|
+    item = Item.create(data)
+    puts "#{item.name} successfully created"
+  end
