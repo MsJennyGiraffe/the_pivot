c3de0e08ca020da02d95be9c079663913a5dffeb
Colin Osborn <Colin.Osborn@gmail.com>
2016-06-12 17:17:56 -0600

    user logout functiional (#78)
    quick test upgrade
    add password field to hide password in views
    removed save_and_open_page
    remove extra test

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -27,4 +27,28 @@ RSpec.feature "User logs in" do
       expect(page).to have_content(user.email)
     end
   end
+
+  scenario "user can logout" do
+    visit root_path
+
+    click_link "Sign Up"
+
+    fill_in "Username", with: user.username
+    fill_in "Email", with: user.email
+    fill_in "Password", with: user.password
+
+    click_button "Create Account"
+
+    expect(current_path).to eq("/dashboard")
+
+    within("nav") do
+      expect(page).to have_content("Logged in as #{user.username}")
+      expect(page).to_not have_content("Login")
+      expect(page).to have_content("Logout")
+    end
+
+    click_link "Logout"
+
+    expect(current_path).to eq("/login")
+  end
 end
