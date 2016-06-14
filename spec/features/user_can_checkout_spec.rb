29e75e4a607c7178ed13b06d5fb0f07aa6c76b3b
saylerb <sayler.b@gmail.com>
2016-06-13 18:09:37 -0600

    remove save and open page

--- spec/features/user_can_checkout_spec.rb
+++ spec/features/user_can_checkout_spec.rb
@@ -1,12 +1,3 @@
-# Background: An existing authenticated user with a cart with items
-# As a visitor
-# When I add items to my cart
-# And I visit "/cart"
-# And when I click "Checkout"
-# Then the order should be placed
-# And my current page should be "/orders"
-# And I should see a message "Order was successfully placed"
-# And I should see the order I just placed in a table
 require 'rails_helper'
 
 RSpec.feature "user can checkout" do
@@ -24,12 +15,11 @@ RSpec.feature "user can checkout" do
 
     expect(current_path).to eq("/cart")
     expect(page).to have_content item.title
-save_and_open_page
+
     click_link "Checkout"
 
     expect(current_path).to eq('/orders')
     expect(page).to have_content("Order was successfully placed")
     expect(page).to have_content("Order Number: #{Order.last.id}")
   end
-
 end
