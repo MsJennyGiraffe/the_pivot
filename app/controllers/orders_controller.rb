6825d8492a9b6b5075ea6be3c5b43ab60aed271e
Jennifer <jennifersoden@hotmail.com>
2016-06-13 20:25:55 -0600

    Alterations
    Changed orders controller to take current user into account during checkout
    Moved remove button inside the qty dropdown
    Minor format changes to item show so the picture is centered and item information displays below

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -4,15 +4,32 @@ class OrdersController < ApplicationController
     @orders = @user.orders
   end
 
+  # def create
+  #   @order = current_user.orders.new(status: "ready")
+  #   if @order.save
+  #     @order.record_order_items(@cart)
+  #     session.delete :cart
+  #     flash[:success] = "Order was successfully placed"
+  #     redirect_to orders_path
+  #   else
+  #     flash.now[:error] = "Please log in to place an order"
+  #     redirect_to login_path
+  #   end
+  # end
+
   def create
-    @order = current_user.orders.new(status: "ready")
-    if @order.save
-      @order.record_order_items(@cart)
-      session.delete :cart
-      flash[:success] = "Order was successfully placed"
-      redirect_to orders_path
+    if current_user
+      @order = current_user.orders.new(status: "ready")
+      if @order.save
+        @order.record_order_items(@cart)
+        session.delete :cart
+        flash[:success] = "Order was successfully placed"
+        redirect_to orders_path
+      else
+        #what would cause an order not to save?
+      end
     else
-      flash.now[:error] = "Please log in to place an order"
+      flash[:warning] = "Please log in to place an order"
       redirect_to login_path
     end
   end
