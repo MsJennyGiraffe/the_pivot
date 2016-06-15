b2b31d5b756b5933b98f280f9d564d81f9701042
Jennifer <jennifersoden@hotmail.com>
2016-06-15 13:33:25 -0600

    fixed line in search controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,7 +2,7 @@ class SearchController < ApplicationController
   def index
     @items = Item.all
     if Item.search(params[:search]).order("title DESC") == []
-      flash[:warning] = "#{params[:search]} not found. Take a look at these items!"
+      flash[:warning] = "#{params[:search]} not found. How about some: "
       @items = Item.featured_home_items(3)
     else
       @items = Item.search(params[:search]).order("title DESC")
