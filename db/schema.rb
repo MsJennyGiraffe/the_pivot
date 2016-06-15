574340386cd0487eacb330db736071e805356adf
Jennifer <jennifersoden@hotmail.com>
2016-06-14 22:54:57 -0600

    added stock

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160611185743) do
+ActiveRecord::Schema.define(version: 20160615035948) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,11 +26,12 @@ ActiveRecord::Schema.define(version: 20160611185743) do
     t.string   "title"
     t.integer  "price"
     t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
     t.string   "image_path"
     t.integer  "category_id"
     t.integer  "weight"
+    t.integer  "stock",       default: 0
   end
 
   add_index "items", ["category_id"], name: "index_items_on_category_id", using: :btree
