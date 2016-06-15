6c4d475a065f3cae1f985fa50b0eb6618e4371a8
Jennifer <jennifersoden@hotmail.com>
2016-06-15 13:31:11 -0600

    added to test for visitor can checkout

--- spec/features/visitor_can_checkout_spec.rb
+++ spec/features/visitor_can_checkout_spec.rb
@@ -30,16 +30,6 @@ RSpec.feature "user can checkout" do
     expect(page).to have_content Order.last.id
   end
   scenario "a visitor without an existing account can checkout" do
-    # As a visitor when I have items in my cart
-    # And when I visit "/cart"
-    # I should not see an option to "Checkout"
-    # I should see an option to "Login or Create Account to Checkout"
-    # After I create an account
-    # And I visit "/cart"
-    # Then I should see all of the data that was there when I was not logged in
-    # When I click "Logout"
-    # Then I should see see "Login"
-    # And I should not see "Logout"
     item = FactoryGirl.create(:item)
     user = FactoryGirl.create(:user)
     visit item_path(item)
@@ -52,18 +42,26 @@ RSpec.feature "user can checkout" do
     click_link "Login or Register to Checkout"
 
     expect(current_path).to eq(login_path)
-    click_link "Create Account"
 
+    click_link "Create Account"
     fill_in "Username", with: user.username
     fill_in "Email", with: user.email
     fill_in "Password", with: user.password
-
     click_button "Create Account"
+
     expect(current_path).to eq(cart_path)
+    expect(page).to have_content(item.title)
+
     click_link "Checkout"
 
     expect(current_path).to eq('/orders')
     expect(page).to have_content("Order was successfully placed")
     expect(page).to have_content Order.last.id
+    expect(page).to_not have_content("Login")
+
+    click_link "Logout"
+
+    expect(page).to have_content("Login")
+    expect(page).to_not have_content("Logout")
   end
 end
