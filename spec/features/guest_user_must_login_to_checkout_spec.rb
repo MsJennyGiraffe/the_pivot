25597e1fe06fb0242658845c79e0164214b5baae
Jennifer <jennifersoden@hotmail.com>
2016-08-17 11:21:54 -0600

    Write test for user cannot bid/checkout unless logged in * Take out method of cart helper that involved old information * Take out old information in cart show * Update item partial to remove old information * Change line in application.rb to remove deprication warning * Change line in environment/test.rb to remove deprication warning. * Add test for user must login to checkout * Changed factories to reflect new changes

---
+++ spec/features/guest_user_must_login_to_checkout_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.feature "Checkout" do
+  scenario "user must be loggin in to bid" do
+    item = create(:item)
+
+    visit "/items"
+    click_link "#{item.title}"
+    fill_in :bid, with: ( item.starting_bid + 1 )
+    click_link "Bid"
+
+    expect(current_path).eq("#{item.username}/#{item.title}")
+    expect(page).to have_content ("Login to Bid")
+  end
+
+  xscenario "user must be loggin in to buyout" do
+    item = create(:item)
+
+    visit "/items"
+    click_link "#{item.title}"
+    click_button "Snap it Up"
+
+    expect(current_path).eq("#{item.username}/#{item.title}")
+    expect(page).to have_content ("Login to Buyout")
+  end
+end
